6cb386b18d3e7c0f65c87b886c07ac043eb1bb04
uhqlA <alfie.alphonce@gmail.com>
2023-01-27 09:17:10 +0300

    layers displaying

--- openlayers/main.js
+++ openlayers/main.js
@@ -18,40 +18,63 @@
   });
 
   map.addLayer(osmTile);
+
+  var CountyDumpsite = new ol.layer.Tile({
+    title: "county dumpsite",
+    source: new ol.source.TileWMS({
+        url: "http://192.168.20.15:8080/geoserver/nrs/wms?service=WMS&version=1.1.0&request=GetMap&layers=nrs%3Aam_sub_county_codes&bbox=36.66450899090029%2C-1.445051569588901%2C37.10500371817151%2C-1.160509578238451&width=768&height=496&srs=EPSG%3A4326&styles=&format=application/openlayers",
+        params: {'LAYERS':'	nrs:us_county_dumpsite', 'TILED':true },
+        serverType: 'geoserver',
+        visibility: true
+    })
+  });
+  map.addLayer(CountyDumpsite);
+
+  var CountyWards= new ol.layer.Tile({
+    title: "county wards",
+    source: new ol.source.TileWMS({
+        url: "http://192.168.20.15:8080/geoserver/nrs/wms?service=WMS&version=1.1.0&request=GetMap&layers=nrs%3Aam_ward_codes&bbox=36.66230773925781%2C-1.446474432945252%2C37.107208251953125%2C-1.15908670425415&width=768&height=496&srs=EPSG%3A4326&styles=&format=application/openlayers",
+        params: {'LAYERS':'	nrs:am_ward_codes', 'TILED':true },
+        serverType: 'geoserver',
+        visibility: true
+    })
+  });
+  map.addLayer(CountyWards);
+
   
 
 //   Adding layer from Geoserver
 
-var SubCounties = new ol.Tile({
-    title: "Sub Counties",
-    source: new ol.source.TileWMS(
-        {
-        url :'http://192.168.20.15:8080/geoserver/web/wicket/bookmarkable/org.geoserver.web.demo.MapPreviewPage?0&filter=false',
-        parameters: {
-            "LAYERS":"nrs:am_sub_county_codes",
-            "TILED":true
-        },
-            serverType: 'geoserver',
-            visibility: true
-    })
-});
+// var SubCounties = new ol.Tile({
+//     title: "Sub Counties",
+//     source: new ol.source.Tile(
+//         {
+//         url :'http://192.168.20.15:8080/geoserver/web/wicket/bookmarkable/org.geoserver.web.demo.MapPreviewPage?0&filter=false',
+//         params: {
+//             "LAYERS":"nrs:am_sub_county_codes",
+//             "TILED":true
+//         },
+//             serverType: 'geoserver',
+//             visibility: true
+//     })
+// });
 
-var CountWards = new ol.Tile({
-    title: "County Wards",
-    source: new ol.source.TileWMS(
-        {
-        url :'http://192.168.20.15:8080/geoserver/web/wicket/bookmarkable/org.geoserver.web.demo.MapPreviewPage?0&filter=false',
-        parameters: {
-            "LAYERS":"nrs:am_ward_codes",
-            "TILED":true
-        },
-            serverType: 'geoserver',
-            visibility: true
-    })
-});
+// var CountMarkets = new ol.layer.Tile({
+//     title: "County Markets",
+//     source: new ol.source.Tile(
+//         {
+//         url :'http://192.168.20.15:8080/geoserver/web/wicket/bookmarkable/org.geoserver.web.demo.MapPreviewPage?0&filter=false',
+//         parames: {
+//             "LAYERS":"nrs:pf_markets",
+//             "TILED":true
+//         },
+//             serverType: 'geoserver',
+//             visibility: true
+//     })
+// });
 
 //adding layer to the map
-// map.addLayer(SubCounties); 
+// map.addLayer(CountyMarkets); 
 
 // adding layer swticher active
 
